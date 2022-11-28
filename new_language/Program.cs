7d17748566da5eeed95d4ca09144db5c5ebc8888
Rectale <vyacheslav-shkoda@mail.ru>
2022-11-28 16:13:19 +0300

    start

---
+++ new_language/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+// комментарий
+// Concole - Позволяет работать с окном терминала
+// .WriteLine("текст") - выводит что-то в окно терминала
+Console.WriteLine("Hello, World! What's up!");
